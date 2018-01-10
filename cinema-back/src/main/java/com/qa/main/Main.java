d0084f952c2936871c0e16edb0754af87b063dda
IxTHING <jordan.white191191@yahoo.co.uk>
2018-01-10 13:29:51 +0000

    addedServerJavaEE

---
+++ cinema-back/src/main/java/com/qa/main/Main.java
@@ -0,0 +1,18 @@
+package com.qa.main;
+
+import com.qa.model.Movie;
+import com.qa.service.Service;
+import com.qa.util.JSONUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Service service = new Service();
+		Movie rambo = new Movie("rambo", "action", "15");
+		service.addMovie(rambo);
+		JSONUtil util = new JSONUtil();
+		String display =util.getJSONForObject(service.getMovieMap());
+		System.out.println("This is the display " + display );
+	}
+
+}
---
+++ cinema-back/src/test/java/com/qa/service/ServiceTest.java
@@ -0,0 +1,27 @@
+package com.qa.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.qa.model.Movie;
+
+public class ServiceTest {
+
+	@Test
+	public void test() {
+		Service service = new Service();
+		Movie rambo = new Movie("rambo", "action", "15");
+		Movie harryPotter = new Movie("harrypotter", "fantasy", "12");
+		Movie it = new Movie("it", "horror", "18");
+		Movie carrie = new Movie("carrie", "horror", "18");
+		service.addMovie(rambo);
+		service.addMovie(harryPotter);
+		service.addMovie(it);
+		service.addMovie(carrie);
+		int count = service.findMovieByGenre("horror");
+		Assert.assertEquals(2, count);
+	}
+
+}
